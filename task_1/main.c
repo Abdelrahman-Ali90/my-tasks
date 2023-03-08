01a8c56ee6ac27cbeafb6b0b4a36e51641341c6b
عبد الرحمن <126777867+Abdelrahman-Ali90@users.noreply.github.com>
2023-03-08 18:35:16 +0200

    Add files via upload

---
+++ task_1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a;
+   printf("inter the number\n");
+   scanf("%d",&a);
+
+   (a % 2 ==0)? printf("the number that you entered is even") : printf("the number that you entered is odd");
+
+    return 0;
+}
