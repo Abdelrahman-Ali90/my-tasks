8e015f459424cc3d8b314928a3c7bf6788776c02
عبد الرحمن <126777867+Abdelrahman-Ali90@users.noreply.github.com>
2023-08-11 04:32:51 +0300

    circle

---
+++ Task11/Q10.py
@@ -0,0 +1,22 @@
+rom math import *
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        area = 3.14159 * pow(self.radius, 2)
+        return area
+    
+    def perimeter(self):
+        perimeter = 2 * 3.14159 * self.radius
+        return perimeter
+
+radius = float(input("Enter the radius of the circle: "))
+
+circle = Circle(radius)
+
+perimeter = circle.perimeter()
+
+print("The area of the circle is:", circle.area())
+print("The perimeter of the circle is:", perimeter)
