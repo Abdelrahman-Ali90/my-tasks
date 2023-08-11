a0f93a03e88a824e91e06998e6577619edbbaa0f
عبد الرحمن <126777867+Abdelrahman-Ali90@users.noreply.github.com>
2023-08-11 19:56:45 +0300

    divide_string

--- Task11/Q3.py
+++ Task11/Q3.py
@@ -1,4 +1,4 @@
-def split_string_in_half(s):
+def divide_string(s):
     length = len(s)
     half_length = length // 2
     first_half = s[:half_length]
@@ -6,13 +6,13 @@ def split_string_in_half(s):
     return first_half, second_half
 
 
-strings = ["apple", "banana", "cherry", "date", "fig", "grape"]
+strings = ["nano", "cool", "cherry", "date", "finger", "zigzag"]
 
 first_halves = []
 second_halves = []
 
 for s in strings:
-    first_half, second_half = split_string_in_half(s)
+    first_half, second_half = divide_string(s)
     first_halves.append(first_half)
     second_halves.append(second_half)
 
