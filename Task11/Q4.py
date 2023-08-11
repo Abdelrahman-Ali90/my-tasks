0f1c26bb6ef14664cddad6503d8bfb38b817ced7
عبد الرحمن <126777867+Abdelrahman-Ali90@users.noreply.github.com>
2023-08-11 20:01:29 +0300

    lucky

--- Task11/Q4.py
+++ Task11/Q4.py
@@ -1,24 +1,23 @@
-def is_lucky_number(number):
-    while number > 0:
-        digit = number % 10
+def lucky_number(number):
+    str_number = str(number)
+    for digit_char in str_number:
+        digit = int(digit_char)
         if digit != 4 and digit != 7:
             return False
-        number //= 10
     return True
 
-def print_lucky_numbers_between(a, b):
+def middle_nums(a, b):
     lucky_numbers = []
     for num in range(a, b + 1):
-        if is_lucky_number(num):
+        if lucky_number(num):  
             lucky_numbers.append(num)
-    
-    if not lucky_numbers:
-        print("-1")
-    else:
-        print(" ".join(map(str, lucky_numbers)))
+    return lucky_numbers
 
-# Get input A and B
-a, b = map(int, input().split())
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
-# Print lucky numbers between A and B
-print_lucky_numbers_between(a, b)
+lucky_nums = middle_nums(num1, num2)
+if not lucky_nums:
+    print("-1")
+else:
+    print(f"The lucky numbers between {num1} and {num2} are {lucky_nums}")
